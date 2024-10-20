04ea566797d07b4b1ca2d0e6de923298d1c5c675
Dina Hussieny <dinahussieny@gmail.com>
2024-10-20 10:01:14 +0300

    Initial Commit

---
+++ I-Freeze/I_FreezeApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_FreezeApp.swift
+//  I-Freeze
+//
+//  Created by Dina Hussieny on 20/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct I_FreezeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
